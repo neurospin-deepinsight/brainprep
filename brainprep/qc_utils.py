01655827142191c823309b0196411b8b4bbe9e8c
LoicDorval <70764594+LoicDorval@users.noreply.github.com>
2022-03-03 11:10:51 +0100

    Update brainprep/qc_utils.py
    Co-authored-by: AGrigis <antoine.grigis@cea.fr>

--- brainprep/qc_utils.py
+++ brainprep/qc_utils.py
@@ -9,7 +9,7 @@
 # for details.
 ##########################################################################
 
-"""usefull functions for cat12vbm correlation based automatic QC.
+"" Usefull functions for cat12vbm correlation based automatic QC.
 
 @author: benoit.dufumier
 @author: julie.victor
