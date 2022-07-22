ce4b7d38e09374a8039b48a21455c46db1cf4c30
JulieNeuro <julie.victor.pro@gmail.com>
2022-07-22 18:13:28 +0200

    add longitudinal option to write matalabatch function in order to keep the bids filetree

--- brainprep/workflow/cat12vbm.py
+++ brainprep/workflow/cat12vbm.py
@@ -62,18 +62,24 @@ def brainprep_cat12vbm(
     """
     print_title("Complete matlab batch...")
     if not isinstance(anatomical, list):
+        print(anatomical)
         anatomical = anatomical.split(",")
+        print(anatomical)
     resource_dir = os.path.join(
         os.path.dirname(brainprep.__file__), "resources")
     batch_file = os.path.join(outdir, "cat12vbm_matlabbatch.m")
     if not longitudinal:
         template_batch = os.path.join(resource_dir, "cat12vbm_matlabbatch.m")
+        print("use matlab batch:", template_batch)
+        brainprep.write_matlabbatch(
+            template_batch, anatomical, tpm, darteltpm, batch_file)
     else:
         template_batch = os.path.join(
             resource_dir, "cat12vbm_matlabbatch_longitudinal.m")
-    print("use matlab batch:", template_batch)
-    brainprep.write_matlabbatch(
-        template_batch, anatomical, tpm, darteltpm, batch_file)
+        print("use matlab batch:", template_batch)
+        brainprep.write_matlabbatch(
+            template_batch, anatomical, tpm, darteltpm, batch_file,
+            longitudinal=True)
 
     print_title("Launch CAT12 VBM matlab batch...")
     cmd = [cat12, "-s", spm12, "-m", matlab, "-b", batch_file]
