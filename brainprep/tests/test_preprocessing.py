631113da68276bc23f34e380cf6fd31d8a6eb984
Antoine Grigis <antoine.grigis@cea.fr>
2022-07-29 16:17:02 +0200

    brainprep/tests/test_preprocessing: add test cases.

--- brainprep/tests/test_preprocessing.py
+++ brainprep/tests/test_preprocessing.py
@@ -11,9 +11,12 @@
 import unittest
 import unittest.mock as mock
 from unittest.mock import patch
-import numpy as np
+import os
+import inspect
 import nibabel
+import numpy as np
 import brainprep
+from brainprep.color_utils import print_title, print_subtitle
 
 
 class TestPreprocessing(unittest.TestCase):
@@ -22,6 +25,7 @@ class TestPreprocessing(unittest.TestCase):
     def setUp(self):
         """ Setup test.
         """
+        os.environ["FSLOUTPUTTYPE"] = "NIFTI_GZ"
         self.popen_patcher = patch("brainprep.utils.subprocess.Popen")
         self.mock_popen = self.popen_patcher.start()
         mock_process = mock.Mock()
@@ -31,51 +35,64 @@ class TestPreprocessing(unittest.TestCase):
         }
         mock_process.configure_mock(**attrs)
         self.mock_popen.return_value = mock_process
-        self.processes = {
-            "scale": (brainprep.scale, {
-                "imfile": "imfile", "scaledfile": "scaledfile", "scale": 1}),
-            "bet": (brainprep.bet2, {
-                "imfile": "imfile", "brainfile": "brainfile", "frac": 0.5,
-                "cleanup": True}),
-            "reorient2std": (brainprep.reorient2std, {
-                "imfile": "imfile", "stdfile": "stdfile"}),
-            "biasfield": (brainprep.biasfield, {
-                "imfile": "imfile", "bfcfile": "bfcfile", "nb_iterations": 3}),
-            "register_affine": (brainprep.register_affine, {
-                "imfile": "imfile", "targetfile": "targetfile",
-                "regfile": "regfile", "cost": "corratio",
-                "interp": "trilinear", "dof": 6}),
-            "apply_affine": (brainprep.apply_affine, {
-                "imfile": "imfile", "targetfile": "targetfile",
-                "regfile": "regfile", "affines": ["trf"], "interp": "spline"}),
-            "apply_mask": (brainprep.apply_mask, {
-                "imfile": "imfile", "maskfile": "maskfile",
-                "genfile": "genfile"}),
-            "recon_all": (brainprep.recon_all, {
-                "fsdir": "fsdir", "anatfile": "anatfile", "sid": "sid"}),
-            "localgi": (brainprep.localgi, {
-                "fsdir": "fsdir", "sid": "sid"})
-        }
+        funcs = inspect.getmembers(brainprep, inspect.isfunction)
+        self.processes = {}
+        for name, _func in funcs:
+            _signature = inspect.getfullargspec(_func)
+            _args = _signature.args
+            _defaults = _signature.defaults or []
+            _defaults = (
+                [None] * (len(_args) - len(_defaults)) + list(_defaults))
+
+            _kwargs = dict((key, val if val is not None else key)
+                           for key, val in zip(_args, _defaults))
+            self.processes[name] = (_func, _kwargs)
+        self.processes["apply_affine"][1]["affines"] = ["trf"]
+        self.processes["deface"][1]["anat_file"] = "sub-XX_T1w.nii.gz"
+        self.processes["tbss_1_preproc"][1]["tbss_dir"] = "/path"
+        self.processes["tbss_1_preproc"][1]["fa_file"] = (
+            "/path/sub-XX_FA.nii.gz")
+        del self.processes["func_connectivity"]
 
     def tearDown(self):
         """ Run after each test.
         """
         self.popen_patcher.stop()
 
+    @mock.patch("builtins.open")
     @mock.patch("nibabel.save")
     @mock.patch("nibabel.load")
     @mock.patch("numpy.savetxt")
     @mock.patch("numpy.loadtxt")
+    @mock.patch("os.chdir")
+    @mock.patch("os.remove")
     @mock.patch("os.path.isdir")
-    def test_processes(self, mock_isdir, mock_loadtxt, mock_savetxt,
-                       mock_load, mock_save):
+    @mock.patch("os.path.isfile")
+    @mock.patch("os.path.islink")
+    def test_processes(self, mock_islink, mock_isfile, mock_isdir, mock_rm,
+                       mock_cd, mock_loadtxt, mock_savetxt, mock_load,
+                       mock_save, mock_open):
         """ Test the processes.
         """
+        print_title("Testing processes...")
         mock_loadtxt.return_value = np.eye(4)
         mock_load.return_value = nibabel.Nifti1Image(
             np.ones((10, 10, 10)), np.eye(4))
+        mock_isfile.return_value = True
+        mock_islink.return_value = True
+        mock_context_manager = mock.Mock()
+        mock_open.return_value = mock_context_manager
+        mock_file = mock.Mock()
+        mock_file.read.return_value = "WRONG"
+        mock_enter = mock.Mock()
+        mock_enter.return_value = mock_file
+        mock_exit = mock.Mock()
+        setattr(mock_context_manager, "__enter__", mock_enter)
+        setattr(mock_context_manager, "__exit__", mock_exit)
         for key, (fct, kwargs) in self.processes.items():
+            print_subtitle(f"{key}...")
             fct(**kwargs)
+        print_title("Done.")
 
 
 if __name__ == "__main__":
